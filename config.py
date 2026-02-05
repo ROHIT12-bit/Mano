e90fe6d44f55d700a788cdaf2d438af3c7be7f3a
ROHIT12-bit <reddyrohit823@gmail.com>
2026-02-05 22:14:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,18 +28,14 @@ class Config:
 
 
     # Default caption
-    DEF_CAP = os.environ.get("DEF_CAP", "<b>{file_name}</b>\n\nPowered By @Botskingdoms")
+    DEF_CAP = os.environ.get("DEF_CAP", "<b>{file_name}</b>\n\n<blockquote>[Powered by Botskingdoms](https://t.me/BOTSKINGDOMS)</blockquote>")
 
     # Messages
-    START_MSG = os.environ.get("START_MSG", "›› ʜᴇʏ!!,  『{mention}』\n\n➤ ᴘᴜʀᴘᴏꜱᴇ ᴏꜰ ᴛʜᴇ ʙᴏᴛ:
-ᴛʜɪꜱ ʙᴏᴛ ᴍᴀᴋᴇꜱ ʀᴇɴᴀᴍɪɴɢ ᴀɴɪᴍᴇ ᴀɴᴅ ꜱᴇʀɪᴇꜱ ꜰɪʟᴇꜱ ᴇᴀꜱʏ ᴀɴᴅ ꜱᴛʀᴇꜱꜱ-ꜰʀᴇᴇ.
-
-‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : @Botskingdoms\n__________________________________")
-    FORCE_SUB_MSG = os.environ.get("FORCE_SUB_MSG", "> ›› ʜᴇʏ 『{mention}』
- ‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ᴜsᴇ ᴛʜᴇ ʙᴏᴛ")
+    START_MSG = os.environ.get("START_MSG", "›› ʜᴇʏ!!,  『{mention}』\n\n➤ ᴘᴜʀᴘᴏꜱᴇ ᴏꜰ ᴛʜᴇ ʙᴏᴛ:\n\nᴛʜɪꜱ ʙᴏᴛ ᴍᴀᴋᴇꜱ ʀᴇɴᴀᴍɪɴɢ ᴀɴɪᴍᴇ ᴀɴᴅ ꜱᴇʀɪᴇꜱ ꜰɪʟᴇꜱ ᴇᴀꜱʏ ᴀɴᴅ ꜱᴛʀᴇꜱꜱ-ꜰʀᴇᴇ.\n\n‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : @Botskingdoms\n__________________________________")
+    FORCE_SUB_MSG = os.environ.get("FORCE_SUB_MSG", "> ›› ʜᴇʏ 『{mention}』\n\n‼️ ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ ʜᴀᴠᴇɴ'ᴛ sᴜʙsᴄʀɪʙᴇᴅ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ʏᴇᴛ, sᴜʙsᴄʀɪʙᴇ ɴᴏᴡ ᴛᴏ ᴜsᴇ ᴛʜᴇ ʙᴏᴛ")
     HELP_MSG = os.environ.get("HELP_MSG", "Send any file and choose option.\nUse /autorename to set format.")
     ABOUT_MSG = os.environ.get("ABOUT_MSG", "<b><blockquote expandable>❍ ᴍʏ ɴᴀᴍᴇ : <a href="https://t.me/AkiraRenameBot">ᴀᴜᴛᴏ ʀᴇɴᴀᴍᴇ</a>
-❍ ᴅᴇᴠᴇʟᴏᴩᴇʀ : <a href="https://t.me/ROHITREDDY69">ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
+❍ ᴅᴇᴠᴇʟᴏᴩᴇʀ : <a href="https://t.me/Rioshin">ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
 ❍ ʟᴀɴɢᴜᴀɢᴇ : <a href="https://www.python.org/">ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
 ❍ ᴅᴀᴛᴀʙᴀꜱᴇ : <a href="https://www.mongodb.com/">ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
 ❍ ʜᴏꜱᴛᴇᴅ ᴏɴ : <a href="https://t.me/botskingdoms">ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
@@ -51,7 +47,7 @@ class Config:
  > ➲ ɪ ᴄᴀɴ ᴀᴜᴛᴏ ʀᴇɴᴀᴍᴇ ʏᴏᴜʀ ʙᴀꜱᴇᴅ ᴏɴ ᴄᴜꜱᴛᴏᴍ ꜱᴇᴛᴛɪɴɢꜱ.
  > ➲ ɪ ꜱᴜᴘᴘᴏʀᴛ ꜱᴇᴀꜱᴏɴ, ᴇᴘɪꜱᴏᴅᴇ, ǫᴜᴀʟɪᴛʏ, ᴀɴᴅ ᴏᴛʜᴇʀ ᴍᴇᴛᴀᴅᴀᴛᴀ ᴀᴜᴛᴏ-ᴇxᴛʀᴀᴄᴛɪᴏɴ.
  > ➲ ɪ ᴄᴀɴ ᴀᴅᴅ ᴄᴜꜱᴛᴏᴍ ᴘᴀꜱꜱᴡᴏʀᴅꜱ and ʙᴀɴɴᴇʀꜱ ᴏɴ ғɪʀsᴛ ᴏʀ ʟᴀsᴛ ᴘᴀɢᴇ.
- > ➲ ꜰᴏʀ ᴍᴏʀᴇ ꜰᴇᴀᴛᴜʀᴇꜱ, ꜱᴇɴᴅ /help ᴄᴏᴍᴍᴀɴᴅ.
+ > ➲ ꜰᴏʀ ᴍᴏʀᴇ ꜰᴇᴀᴛᴜʀᴇꜱ, ᴄʜᴇᴄᴋ ᴄᴏᴍᴍᴀɴᴅs.
 
 ➲ ꜰᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴛʜɪꜱ ʙᴏᴛ ᴄᴏɴᴛᴀᴄᴛ ᴛʜᴇ ᴄʀᴇᴀᴛᴏʀ")
 
@@ -61,7 +57,7 @@ class Config:
     CANCEL_SEQUENCE_MSG = os.environ.get("CANCEL_SEQUENCE_MSG", "Sequence cancelled!")
 
     # Credits line
-    CREDITS_LINE = os.environ.get("CREDITS_LINE", "Powered By Botskingdoms")
+    CREDITS_LINE = os.environ.get("CREDITS_LINE", "<blockquote>[Powered by Botskingdoms](https://t.me/BOTSKINGDOMS)</blockquote>")
 
     # Logs channel
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
